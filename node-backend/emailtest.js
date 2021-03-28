731ac191f9b4726cf71982645441a4405649a791
alfredwang0707 <alfredwang0707@gmail.com>
2021-03-27 20:42:54 -0400

    add env file

--- node-backend/emailtest.js
+++ node-backend/emailtest.js
@@ -1,7 +1,5 @@
-export MJ_APIKEY_PUBLIC='73098aba62ed23503c72ad28e4d3ac4c'
-export MJ_APIKEY_PRIVATE='b71cbd507ba7ef650a29cee6d7342380'
 
-*/
+
 const mailjet = require('node-mailjet').connect(
   process.env.MJ_APIKEY_PUBLIC,
   process.env.MJ_APIKEY_PRIVATE
